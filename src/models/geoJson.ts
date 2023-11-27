5022a8b1ccc20a2098ee3ed41b80a7ca00e5e0df
Thiago Santana <thi.dimond1@gmail.com>
2023-11-27 18:13:40 -0300

    Add query for searching the nearest partner.

--- src/models/geoJson.ts
+++ src/models/geoJson.ts
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+import mongoose, { PipelineStage } from "mongoose";
 
 export const addressSchema = new mongoose.Schema({
   type: {
@@ -15,4 +15,27 @@ export const addressSchema = new mongoose.Schema({
 export const coverageAreaSchema = new mongoose.Schema({
   type: { type: String, enum: ["MultiPolygon"], required: true },
   coordinates: { type: [[[[Number]]]], required: true },
+});
+
+export const nearestPartnerQuery = (
+  coordinates: [number, number]
+): PipelineStage => ({
+  $geoNear: {
+    near: {
+      type: "Point",
+      coordinates,
+    },
+    spherical: true,
+    distanceField: "distanceInMeters",
+    query: {
+      coverageArea: {
+        $geoIntersects: {
+          $geometry: {
+            type: "Point",
+            coordinates,
+          },
+        },
+      },
+    },
+  },
 });
