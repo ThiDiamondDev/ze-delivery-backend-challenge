c94aeb753c0c5826ef23e49d86b083bdbb977f02
Thiago Santana <thi.dimond1@gmail.com>
2023-11-27 18:17:54 -0300

    Add test partner data.

---
+++ src/tests/integration/testPartnerData.ts
@@ -0,0 +1,32 @@
+export const testPartnerData = {
+  "id": 1,
+  "tradingName": "Test Partner",
+  "ownerName": "Tester",
+  "document": "0000000000/0001",
+  "coverageArea": {
+    "type": "MultiPolygon",
+    "coordinates": [
+      [
+        [
+          [30, 20],
+          [45, 40],
+          [10, 40],
+          [30, 20],
+        ],
+      ],
+      [
+        [
+          [15, 5],
+          [40, 10],
+          [10, 20],
+          [5, 10],
+          [15, 5],
+        ],
+      ],
+    ],
+  },
+  "address": {
+    "type": "Point",
+    "coordinates": [30, 20],
+  },
+};
